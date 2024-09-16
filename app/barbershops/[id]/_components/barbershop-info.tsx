861cc73a2e91448094b9e988b1de38f679b8e436
ENDERSON-MARIN <marinenderson1@gmail.com>
2024-09-16 04:50:11 -0300

    feat: add ServiceItem component

--- app/barbershops/[id]/_components/barbershop-info.tsx
+++ app/barbershops/[id]/_components/barbershop-info.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import { Button } from "@/app/_components/ui/button";
-import { ChevronLeftIcon, MenuIcon } from "lucide-react";
+import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
 import { Barbershop } from "@prisma/client";
 import { useRouter } from "next/navigation";
 
@@ -10,7 +10,7 @@ interface BarbershopInfoProps {
   barbershop: Barbershop;
 }
 
-const BarbershopInfo = async ({ barbershop }: BarbershopInfoProps) => {
+const BarbershopInfo = ({ barbershop }: BarbershopInfoProps) => {
   const router = useRouter();
 
   const handleBackClick = () => {
@@ -42,6 +42,17 @@ const BarbershopInfo = async ({ barbershop }: BarbershopInfoProps) => {
           style={{ objectFit: "cover" }}
           className="opacity-75"
         />
+      </div>
+      <div className="px-5 pt-3 pb-6 border-b border-solid border-secondary">
+        <h1 className="text-xl font-bold">{barbershop.name}</h1>
+        <div className="flex items-center gap-1 mt-2">
+          <MapPinIcon className="text-primary" size={18} />
+          <p className="text-sm">{barbershop.address}</p>
+        </div>
+        <div className="flex items-center gap-1 mt-2">
+          <StarIcon className="text-primary" size={18} />
+          <p className="text-sm">5,0 (899 avaliaçoes)</p>
+        </div>
       </div>
     </div>
   );
